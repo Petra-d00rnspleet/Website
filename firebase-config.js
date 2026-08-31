80f313bbef1e9494a89727430ef7794c078119f1
Petra-d00rnspleet <58157351@leerling.agoramaasenpeel.nl>
2026-08-31 18:10:04 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// Vul hier de gegevens van jouw eigen Firebase-project in.
+// Je vindt deze in de Firebase Console onder:
+// Project instellingen -> Algemeen -> Jouw apps -> SDK setup and configuration
+
+const firebaseConfig = {
+  apiKey: "VUL-HIER-IN",
+  authDomain: "VUL-HIER-IN.firebaseapp.com",
+  databaseURL: "https://VUL-HIER-IN-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "VUL-HIER-IN",
+  storageBucket: "VUL-HIER-IN.appspot.com",
+  messagingSenderId: "VUL-HIER-IN",
+  appId: "VUL-HIER-IN"
+};
+
+firebase.initializeApp(firebaseConfig);
